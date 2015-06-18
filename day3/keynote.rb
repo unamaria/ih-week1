d39890e9d5c8ef15d69672a8350b4911c49a559a
Maria <unamaria@gmail.com>
2015-06-18 16:27:52 +0200

    First iteration kind of working (before Slide class)

--- day3/keynote.rb
+++ day3/keynote.rb
@@ -15,7 +15,6 @@ class TextFileParser
     end
     slides
   end
-
 end
 
 class Terminal
@@ -24,11 +23,11 @@ class Terminal
   end
 
   def height
-    height = @term_info[0]
+    @term_info[0]
   end
 
   def width
-    width = @term_info[1]
+    @term_info[1]
   end
 end
 
@@ -36,31 +35,31 @@ class Presentation
   def initialize(slides)
     @slides = slides
     @current_slide = 0
+    @term = Terminal.new
   end
 
   def place_slide(slide)
-    term = Terminal.new
-    vertical = term.height / 2 - 1
-    horizontal = (term.width - slide.length) / 2
-    slide = ("\n" * (vertical / 2)) +  " " * horizontal + slide + ("\n" * (vertical / 2))
+    vertical = @term.height / 2 - 1
+    horizontal = (@term.width - slide.length) / 2
+    ("\n" * vertical) +  " " * horizontal + slide + ("\n" * vertical)
   end
 
   def show_slide
     puts place_slide(@slides[@current_slide])
   end
 
-  def show
-    show_slide
-    if get_input == "next"
-      @current_slide += 1
-      show_slide
-      get_input
-    elsif input == "previous"
-      @current_slide -= 1
-      show_slide
-      get_input
-    else 
-      puts "Sorry, you can either type previous or next"
+  def navigation
+    while @current_slide < @slides.length
+      input = get_input
+      if input == "next"
+        @current_slide += 1
+        show_slide
+      elsif input == "previous"
+        @current_slide -= 1
+        show_slide
+      else 
+        puts "Sorry, you can either type previous or next"
+      end
     end 
   end
 
@@ -70,7 +69,12 @@ class Presentation
 
   def get_input
     ask_input
-    input = gets.chomp
+    gets.chomp
+  end
+
+  def show
+    show_slide
+    navigation
   end
 
 end
